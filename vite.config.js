7740412ba7ce841252a7c7145ad993a35909f1ee
krishna25102002 <tripdaries01@gmail.com>
2025-10-27 22:20:20 +0530

    fixed for hosting in render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',             // Make sure it's accessible externally
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      'event-zone-admin-dashboard.onrender.com'  // ✅ Add your Render domain here
+    ]
+  }
 })
